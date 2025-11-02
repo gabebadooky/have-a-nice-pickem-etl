0a2bccd18ab2c489f1c9421fed80728449289469
Gabriel Baduqui <baduquig@gmail.com>
2025-11-01 22:41:21 -0700

    develop extract.fox.scheduleweek package

---
+++ etl/extract/fox/scheduleweek.go
@@ -0,0 +1,23 @@
+/*
+Package that:
+  - Scrapes the FOX Schedule page HTML for a given week number
+*/
+package fox
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func ScheduleWeek(league string, weeknum uint8) *http.Response {
+	var schedulePageLink string = fmt.Sprintf("https://www.foxsports.com/college-football/schedule?groupId=2&seasonType=reg&week=%d", weeknum)
+
+	resp, err := http.Get(schedulePageLink)
+	if err != nil {
+		log.Printf("Error occurred navigating to %s:\n%s", schedulePageLink, err)
+	}
+	defer resp.Body.Close()
+
+	return resp
+}
