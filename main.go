04b4a4f1b842f9b4557abae71667193a1391a284
Gabriel Baduqui <baduquig@gmail.com>
2026-01-03 11:12:32 -0700

    load package WIP...

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"have-a-nice-pickem-etl/etl/extract"
-)
-
 func main() {
-	var week uint = 7
-	var league string = "CFB"
 
-	if league == "CFB" {
-		games := extract.ExtractGames(extract.CfbGamesExtract{Week: week})
-		teams := extract.ExtractTeams(extract.CfbTeamsExtract{Week: week})
-		locations := extract.ExtractLocations(extract.CfbLocationsExtract{Week: week})
-	}
 }
