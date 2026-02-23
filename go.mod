e9bbca8c25d7a9411b5dac4329d20dc3c0d43647
Gabriel Baduqui <baduquig@gmail.com>
2026-02-22 21:57:27 -0700

    develop function to instantiate connection to Postgres database WIP...

--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module have-a-nice-pickem-etl
 go 1.25.2
 
 require (
-	github.com/PuerkitoBio/goquery v1.10.3 // indirect
+	github.com/PuerkitoBio/goquery v1.10.3
+	github.com/jackc/pgx/v5 v5.8.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.3 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
 )
--- internal/load/load.go
+++ internal/load/load.go
@@ -3,6 +3,7 @@
 package load
 
 import (
+	"context"
 	"encoding/csv"
 	"fmt"
 	"have-a-nice-pickem-etl/internal/transform/bettingodds"
@@ -14,6 +15,8 @@ import (
 	"have-a-nice-pickem-etl/internal/transform/teamdetails"
 	"log"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 // instantiateCsvWriter creates and returns a CSV writer for the given file path.
@@ -28,12 +31,27 @@ func instantiateCsvWriter(filepath string) (*os.File, *csv.Writer) {
 	return f, csvwriter
 }
 
+func callBulkLoadProcedure(queryString string) {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Printf("Error occurred instantiating Postgres Database connection: %s", err)
+	}
+	defer conn.Close(context.Background())
+
+	_, err = conn.Exec(context.Background(), "%s", queryString)
+	if err != nil {
+		log.Printf("Error occurred with the SQL command %s: %s", queryString, err)
+	}
+}
+
 // BettingOdds writes betting odds records to data/bettingodds.csv.
 func BettingOdds(records []bettingodds.BettingOdds, csvLoadFolderPath string) {
 	bulkDataLoadFilePath := fmt.Sprintf("%s/%s", csvLoadFolderPath, "bettingodds.csv")
 	f, w := instantiateCsvWriter(bulkDataLoadFilePath)
 	defer f.Close()
 	defer w.Flush()
+
+	log.Printf("Writing Betting Odds records to %s", bulkDataLoadFilePath)
 
 	for _, record := range records {
 		w.Write([]string{
@@ -50,6 +68,9 @@ func BettingOdds(records []bettingodds.BettingOdds, csvLoadFolderPath string) {
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := fmt.Sprintf("CALL %s('%s')", "bulk_load_betting_odds", bulkDataLoadFilePath)
+	callBulkLoadProcedure(queryString)
 }
 
 // Boxscores writes boxscore records to data/boxscores.csv.
@@ -58,6 +79,8 @@ func Boxscores(records []boxscore.Boxscore, csvLoadFolderPath string) {
 	f, w := instantiateCsvWriter(bulkDataLoadFilePath)
 	defer f.Close()
 	defer w.Flush()
+
+	log.Printf("Writing Boxscores records to %s", bulkDataLoadFilePath)
 
 	for _, record := range records {
 		w.Write([]string{
@@ -75,6 +98,9 @@ func Boxscores(records []boxscore.Boxscore, csvLoadFolderPath string) {
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := fmt.Sprintf("CALL %s('%s')", "bulk_load_box_scores", bulkDataLoadFilePath)
+	callBulkLoadProcedure(queryString)
 }
 
 // GameDetails writes game detail records to data/gamedetails.csv.
@@ -83,6 +109,8 @@ func GameDetails(records []gamedetails.GameDetails, csvLoadFolderPath string) {
 	f, w := instantiateCsvWriter(bulkDataLoadFilePath)
 	defer f.Close()
 	defer w.Flush()
+
+	log.Printf("Writing Game Details records to %s", bulkDataLoadFilePath)
 
 	for _, record := range records {
 		w.Write([]string{
@@ -106,6 +134,9 @@ func GameDetails(records []gamedetails.GameDetails, csvLoadFolderPath string) {
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := fmt.Sprintf("CALL %s('%s')", "bulk_load_games", bulkDataLoadFilePath)
+	callBulkLoadProcedure(queryString)
 }
 
 // Stats writes game stats records to data/stats.csv.
@@ -114,6 +145,8 @@ func Stats(records []gamestats.GameStats, csvLoadFolderPath string) {
 	f, w := instantiateCsvWriter(bulkDataLoadFilePath)
 	defer f.Close()
 	defer w.Flush()
+
+	log.Printf("Writing Stats records to %s", bulkDataLoadFilePath)
 
 	for _, record := range records {
 		var gameID string = record.GameID
@@ -133,6 +166,9 @@ func Stats(records []gamestats.GameStats, csvLoadFolderPath string) {
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := fmt.Sprintf("CALL %s('%s')", "bulk_load_locations", bulkDataLoadFilePath)
+	callBulkLoadProcedure(queryString)
 }
 
 // LocationDetails writes location detail records to data/locations.csv.
@@ -141,6 +177,8 @@ func LocationDetails(records []locationdetails.LocationDetails, csvLoadFolderPat
 	f, w := instantiateCsvWriter(bulkDataLoadFilePath)
 	defer f.Close()
 	defer w.Flush()
+
+	log.Printf("Writing Location Details records to %s", bulkDataLoadFilePath)
 
 	for _, record := range records {
 		w.Write([]string{
@@ -156,6 +194,9 @@ func LocationDetails(records []locationdetails.LocationDetails, csvLoadFolderPat
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := fmt.Sprintf("CALL %s('%s')", "bulk_load_stats", bulkDataLoadFilePath)
+	callBulkLoadProcedure(queryString)
 }
 
 // TeamRecord writes team record rows to data/teamrecords.csv.
@@ -164,6 +205,8 @@ func TeamRecord(records []record.Record, csvLoadFolderPath string) {
 	f, w := instantiateCsvWriter(bulkDataLoadFilePath)
 	defer f.Close()
 	defer w.Flush()
+
+	log.Printf("Writing Team Record rows to %s", bulkDataLoadFilePath)
 
 	for _, record := range records {
 		w.Write([]string{
@@ -178,6 +221,9 @@ func TeamRecord(records []record.Record, csvLoadFolderPath string) {
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := fmt.Sprintf("CALL %s('%s')", "bulk_load_team_records", bulkDataLoadFilePath)
+	callBulkLoadProcedure(queryString)
 }
 
 // TeamDetails writes team detail records to data/teams.csv.
@@ -186,6 +232,8 @@ func TeamDetails(records []teamdetails.TeamDetails, csvLoadFolderPath string) {
 	f, w := instantiateCsvWriter(bulkDataLoadFilePath)
 	defer f.Close()
 	defer w.Flush()
+
+	log.Printf("Writing Team Details records to %s", bulkDataLoadFilePath)
 
 	for _, record := range records {
 		w.Write([]string{
@@ -207,4 +255,7 @@ func TeamDetails(records []teamdetails.TeamDetails, csvLoadFolderPath string) {
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := fmt.Sprintf("CALL %s('%s')", "bulk_load_teams", bulkDataLoadFilePath)
+	callBulkLoadProcedure(queryString)
 }
