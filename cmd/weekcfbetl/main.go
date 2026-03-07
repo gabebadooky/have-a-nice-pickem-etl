e79624d6d9ed66924b5af1cc2490b6f0f6dd0228
Gabriel Baduqui <baduquig@gmail.com>
2026-03-06 22:10:15 -0700

    update main package to include command-line flag for week extraction and load environment variables

--- cmd/weekcfbetl/main.go
+++ cmd/weekcfbetl/main.go
@@ -1,14 +1,21 @@
-package weekcfbetl
+package main
 
 import (
+	"flag"
 	"have-a-nice-pickem-etl/internal/extract"
 	"have-a-nice-pickem-etl/internal/load"
 	"have-a-nice-pickem-etl/internal/transform"
+
+	"github.com/joho/godotenv"
 )
 
 // Runs the ETL pipeline for a given weeks games, teams, and locations
-func main(week uint) {
-	weekExtract := extract.CfbExtract{Week: week}.PerformExtract()
+func main() {
+	godotenv.Load()
+
+	week := flag.Uint("week", 1, "The week to extract")
+	flag.Parse()
+	weekExtract := extract.CfbExtract{Week: *week}.PerformExtract()
 	weekTransformation := transform.New{Extract: weekExtract}.PerformTransformation()
 	load.New{Transformation: weekTransformation}.PerformLoad()
 }
