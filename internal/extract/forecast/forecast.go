7ebdf2bfa02301e7ad310fa5ccb1e6f1dd4d9f52
Gabriel Baduqui <baduquig@gmail.com>
2026-04-15 10:40:03 -0700

    Develop forecast package

--- internal/extract/forecast/forecast.go
+++ internal/extract/forecast/forecast.go
@@ -1,1 +1,32 @@
 package forecast
+
+import "have-a-nice-pickem-etl/internal/extract/forecast/openweathermap"
+
+type OpenWeatherForecast struct {
+	LocationID   string
+	ZuluGameTime string
+	Lat          float32
+	Lon          float32
+}
+
+type Forecast struct {
+	LocationID   string
+	ZuluGameTime string
+	OpenWeather  openweathermap.OpenWeatherMapEndpoint
+}
+
+func (w OpenWeatherForecast) GetForecast() Forecast {
+	openWeatherTimestampForecast := openweathermap.OpenWeatherTimestampForecast{
+		Lat:          w.Lat,
+		Lon:          w.Lon,
+		ZuluGameTime: w.ZuluGameTime,
+	}
+
+	openWeatherForecastDetails := openWeatherTimestampForecast.GetForecastDetails()
+
+	return Forecast{
+		LocationID:   w.LocationID,
+		ZuluGameTime: w.ZuluGameTime,
+		OpenWeather:  openWeatherForecastDetails,
+	}
+}
